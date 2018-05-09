f17f6415257dfea2ec688675c37fa7d21f5a2c31
lenin <vargaslen@gmail.com>
2018-05-09 10:33:26 -0430

    Estado inicial de archivos en Blog

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,26 @@
+class ArticlesController<ApplicationController
+  #GET /articles
+  def index
+    @articles = Article.all
+  end
+
+  #GET /articles/:id
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  #GET  /articles/new
+  def new
+    @article  = Article.new
+  end
+
+  #POST  /articles
+  def create
+    @article  = Article.new(title: params[:article][:title],body: params[:article][:body])
+    if @article.save then
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+
+  resources :articles
+=begin
+    get "/articles"  index
+    post "/articles"  create
+    delete "/articles"  delete
+    get "/articles/:id"  show
+    get "/articles/new"  new
+    get "/articles/:id/edit"  edit
+    put "/articles/:id"  update
+    patch "/articles/:id"  update
+=end
+
+  get 'welcome/index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+end
